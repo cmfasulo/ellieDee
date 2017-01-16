60dc499f57ff2af88090baa301e98e59336167eb
Chris Fasulo <fasulo810@gmail.com>
2017-01-15 23:24:11 -0800

    started reworking entire app

--- assets/js/ellieDee.js
+++ assets/js/ellieDee.js
@@ -1,67 +1,62 @@
-// Set interval for logo flash below (colorChange function)
-// var x;
-// function colorChange() {
-//     x = 1;
-//     setInterval(flashLogo, 1000);
+$(document).ready(function() {
+  buildMatrix();
+});
+
+//Creates a function that generates a really long string with entire table HTML
+function buildMatrix() {
+  var matrixString = "";
+  var ledNum = 143;
+  for (var i = 11; i >= 0; i--) {
+    matrixString += "<tr id='" + i + "'>" //creates opening tag and id for current row
+    for (var j = 11; j >= 0; j--) {
+      matrixString += "<td id='" + ledNum + "'><div></div></td>" //adds the current column to the string for the current row; moves onto the next column in the inner "for" loop
+      ledNum--;
+    }
+    matrixString += "</tr>"; //adds a closing tag for the current row; moves onto the next row in the outer "for" loop
+  }
+  $("#ledMatrix").append(matrixString);
+}
+
+
+// function mouseDown(clicked_id) {
+//    var setColor = document.getElementById(clicked_id).backgroundColor;
+//    istrue = true;
+//    timer = setTimeout(function(){ grabColor(clicked_id);},delay);
 // }
-
-// Connection Status indicator light "behind" LED Matrix bottom logo
-// function flashLogo () {
-// 	if (x === 1) {
-//         color = "red";
-//         x = 2;
-//     } else {
-//         color = "green";
-//         x = 1;
-//     }
-//     document.getElementById("logoBackground").style.backgroundColor = color;
+//
+// function grabColor(clicked_id) {
+//       if(timer)
+// 	      clearTimeout(timer);
+//
+//       if(istrue) {
+// 	      document.getElementById("currentColor").style.backgroundColor = document.getElementById(clicked_id).style.backgroundColor;
+//       }
 // }
-
-// Change LED Matrix buttons to selected color from Color Picker
-
-var timer;
-var istrue = false;
-var delay = 1000; // how much long u have to hold click in MS
-
-function mouseDown(clicked_id) {
-   var setColor = document.getElementById(clicked_id).backgroundColor;
-   istrue = true;
-   timer = setTimeout(function(){ grabColor(clicked_id);},delay);
-}
-
-function grabColor(clicked_id) {
-      if(timer)
-	      clearTimeout(timer);
-      
-      if(istrue) {
-	      document.getElementById("currentColor").style.backgroundColor = document.getElementById(clicked_id).style.backgroundColor;
-      }
-}
-
-function reset() {
-   istrue =false;
-}
-
-function applyColor (clicked_id) {
-
-	var ledID = clicked_id;
-	var setColor = document.getElementById('currentColor').style.backgroundColor; //example: rgb(0,0,0);
-	var rgb = setColor.replace(/[^\d,]/g, '').split(','); //strips rgb into values only, example: (0,0,0);
-	var r = rgb[0];
-	var g = rgb[1];
-	var b = rgb[2];
-
-	document.getElementById(clicked_id).style.backgroundColor = setColor;
-}
-
-// Reset all LED Matrix buttons to default color
-function resetAll () {
-	for (var i = 1; i <= 144; i++) {
-		document.getElementById(i).style.backgroundColor = "buttonface";
-	}
-}
-
-// Reset current color to "black" or "off" to turn off indivdual LEDs
-function resetPicker () {
-	document.getElementById("currentColor").style.backgroundColor = "#000";
-}
+//
+// function reset() {
+//    istrue =false;
+// }
+//
+// function applyColor (clicked_id) {
+//
+// 	var ledID = clicked_id;
+// 	var setColor = document.getElementById('currentColor').style.backgroundColor; //example: rgb(0,0,0);
+// 	var rgb = setColor.replace(/[^\d,]/g, '').split(','); //strips rgb into values only, example: (0,0,0);
+// 	var r = rgb[0];
+// 	var g = rgb[1];
+// 	var b = rgb[2];
+//
+// 	document.getElementById(clicked_id).style.backgroundColor = setColor;
+// }
+//
+// // Reset all LED Matrix buttons to default color
+// function resetAll () {
+// 	for (var i = 1; i <= 144; i++) {
+// 		document.getElementById(i).style.backgroundColor = "buttonface";
+// 	}
+// }
+//
+// // Reset current color to "black" or "off" to turn off indivdual LEDs
+// function resetPicker () {
+// 	document.getElementById("currentColor").style.backgroundColor = "#000";
+// }
