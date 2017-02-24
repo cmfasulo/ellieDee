695de12c28a67b3ab372c9684e4ea81f3f2ac987
Chris Fasulo <fasulo810@gmail.com>
2017-02-23 20:44:30 -0800

    fixed devise views for updating user info

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :drawings
-  devise_for :users
-  resources :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  # resources :users
 
 
   get '/' => 'ellie_dee#index'
